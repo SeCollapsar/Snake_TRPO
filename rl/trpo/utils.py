c2394e0a04b7e5fa873c99df478bf1ce0cf7aabc
se1collapsar <1902018278@qq.com>
2026-04-20 17:24:00 +0800

    delete unused file 20-04-2026 17-24

--- rl/trpo/utils.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-
-def flat_grad(grads):
-    return np.concatenate([g.flatten() for g in grads])
-
-
-def kl_divergence(old_probs, new_probs):
-    return np.sum(old_probs * (np.log(old_probs + 1e-8) - np.log(new_probs + 1e-8)))    
